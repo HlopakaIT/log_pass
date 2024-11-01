453cf10da5bc102bf01ac7bbbc44caed067d2e24
HlopakA <faka2@ukr.net>
2024-11-01 11:25:14 +0300

    log_pass v 0.11

--- log_pass.js
+++ log_pass.js
@@ -1,19 +1,20 @@
 const submit = document.getElementById("submit");
 
-const username = document.querySelector("#username")
-
-submit.addEventListener("onclick", createTwoElements)
+submit.addEventListener("submit", createTwoElements)
 
 function createTwoElements() {
-
-
-  
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("username").value;
-    console.log(username);
-    console.log(password);
+    const data = document.createElement("p");
+    data.innerHTML = "Username:";
+    userdata.append(data);
 }
+
+
 // const userdata = document.getElementById("userdata");
 // const data = document.createElement("p");
 // data.innerHTML = "Username:";
 // userdata.append(data);
+
+// const username = document.getElementById("username").value;
+// const password = document.getElementById("username").value;
+// console.log(username);
+// console.log(password);
