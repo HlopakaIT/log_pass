a0bd81d9fa073affa71a222d78734741425aedee
HlopakA <faka2@ukr.net>
2024-11-02 06:18:25 +0300

    log_pass v 1.0 not_clear

--- log_pass.js
+++ log_pass.js
@@ -1,12 +1,95 @@
-const submit = document.getElementById("submit");
+const submit = document.getElementById("form");
 
 submit.addEventListener("submit", createTwoElements)
 
-function createTwoElements() {
-    const data = document.createElement("p");
-    data.innerHTML = "Username:";
-    userdata.append(data);
+function createTwoElements(event) {
+    event.preventDefault()
+
+
+    if(document.getElementById("user")) {
+
+        const username = document.getElementById("username").value
+        const password = document.getElementById("password").value
+
+        const NewElement = document.getElementById("user")
+        const NewElementTwo = document.getElementById("pass")
+
+        NewElement.innerHTML = "login: " + username;
+        NewElementTwo.innerHTML = "password: " + password; 
+
+    } else {
+        const newElement = document.createElement("p");
+        const newElementTwo = document.createElement("p");
+
+        newElement.setAttribute( "id", "user" );
+        newElementTwo.setAttribute( "id", "pass" );
+    
+        const username = document.getElementById("username").value
+        const password = document.getElementById("password").value
+    
+        newElement.innerHTML = "login: " + username;
+        newElementTwo.innerHTML = "password: " + password; 
+    
+        userdata.append(newElement);
+        userdata.append(newElementTwo);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const bla = document.getElementById("bla");
+    // const blu = document.getElementById("blu");
+
+    // if(bla) {
+    //     data.remove();
+    //     datatwo.remove();
+    //     const data = document.createElement("p");
+    //     const datatwo = document.createElement("p");
+    //     data.innerHTML = "login: ";
+    //     datatwo.innerHTML = "password: ";
+    //     userdata.append(data);
+    //     userdata.append(datatwo);
+
+    // } else {
+        
+
+
+    //     data.setAttribute( "id", "bla" );
+    //     datatwo.setAttribute( "id", "blu");
+
+    //     const username = document.getElementById("username").value;
+    //     const password = document.getElementById("password").value;
+
+    //     data.innerHTML = "login: " + username;
+    //     datatwo.innerHTML = "password: " + password;
+
+    //     userdata.append(data);
+    //     userdata.append(datatwo);
+//     }
+// }
 
 
 // const userdata = document.getElementById("userdata");
